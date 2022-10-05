6804f820e256c84778a565146aeeec8727b1f5ad
Márk Megyeri <megyko1234@gmail.com>
2022-10-05 14:43:40 +0200

    Felhasználók felvérele (CRUD művelet beállítása)

--- config/laratrust_seeder.php
+++ config/laratrust_seeder.php
@@ -12,21 +12,19 @@ return [
     'truncate_tables' => true,
 
     'roles_structure' => [
-        'superadministrator' => [
+        'admin' => [
             'users' => 'c,r,u,d',
             'payments' => 'c,r,u,d',
             'profile' => 'r,u'
         ],
-        'administrator' => [
+        'webmester' => [
             'users' => 'c,r,u,d',
             'profile' => 'r,u'
         ],
-        'user' => [
+        'felhasznalo' => [
             'profile' => 'r,u',
-        ],
-        'role_name' => [
-            'module_1_name' => 'c,r,u,d',
         ]
+       
     ],
 
     'permissions_map' => [
